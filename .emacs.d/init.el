9ad28b62984726f6f893b4e64941782dd1cf7551
Bruno G. Ciccarino <bgrecaciccarino@gmail.com>
2026-07-23 21:54:51 -0300

    fix: fixing emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,6 +13,7 @@
 (setq inhibit-startup-message t)
 (setq display-line-numbers-type 'relative)
 
+(global-visual-line-mode 1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (tooltip-mode -1)
@@ -24,6 +25,7 @@
   :config
   (load-theme 'gruvbox-dark-medium t))
 
+;; my old beloved themme ;-;
 ;; (load-theme 'sexy t)
 
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
@@ -90,6 +92,52 @@
       (when (string-match-p "-autoloads.el" name)
 	(kill-buffer buffer)
 	(message "Killed autoloads buffer %s" name)))))
+
+;; Org mode config
+(setq org-directory "~/bgc/agenda/org/")
+
+;; Org Agenda usage:
+;; M-x org-agenda (or bind a key, e.g. C-c a)
+;;
+;; Inside the agenda view:
+;;   t  -> change TODO state (TODO -> DOING -> WAITING -> DONE)
+;;   ,  -> set priority (A/B/C)
+;;   s  -> schedule the item for a date
+;;   d  -> set a deadline
+;;   r  -> refresh/reload the agenda
+;;   q  -> quit
+
+(setq org-agenda-files
+      (list (concat org-directory "tasks.org")
+	    (concat org-directory "notes.org")
+	    (concat org-directory "journal.org")))
+
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "DOING(i)" "WAITING(w)" "|" "DONE(d)" "CANCELED(c)")))
+
+(setq org-capture-templates
+      `(("t" "Task" entry (file+headline ,(concat org-directory "tasks.org") "Inbox")
+         "* TODO %?\n  %U\n  %a")
+        ("n" "Note" entry (file ,(concat org-directory "notes.org"))
+         "* %?\n  %U")
+        ("j" "Journal" entry (file+datetree ,(concat org-directory "journal.org"))
+         "* %U %?\n")))
+
+(global-set-key (kbd "C-c c") #'org-capture)
+(global-set-key (kbd "C-c a") #'org-agenda)
+
+(setq org-log-done 'time)
+(setq org-log-into-drawer t)
+
+(setq org-agenda-span 'week)
+(setq org-agenda-start-on-weekday 1)
+(setq org-agenda-skip-scheduled-if-done t)
+(setq org-agenda-skip-deadline-if-done t)
+
+(setq org-priority-faces
+      '((?A . (:foreground "#fb4934" :weight bold))
+        (?B . (:foreground "#fabd2f"))
+        (?C . (:foreground "#8ec07c"))))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
