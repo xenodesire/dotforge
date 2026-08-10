1301e498f79a0b905d11bf51a5a1c9fc6facbd37
Bruno G. Ciccarino <bgrecaciccarino@gmail.com>
2026-08-10 02:53:50 -0400

    feat: new rice theme

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -13,7 +13,7 @@ hl.on("hyprland.start", function()
   hl.exec_cmd(terminal)
   hl.exec_cmd("nm-applet")
   hl.exec_cmd("waybar")
-  hl.exec_cmd("swaybg -m fill -i $HOME/.config/wallpapers/od_rice.png")
+  hl.exec_cmd("swaybg -m fill -i $HOME/.config/wallpapers/wallpaper-retro-onedark.png")
 end)
 
 hl.env("XCURSOR_SIZE", "24")
